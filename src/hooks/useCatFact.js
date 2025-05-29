fda83e17305c690676fc30d466be3051890264ca
Juankyyy <juancamiloherrera960@gmail.com>
2025-05-29 14:45:45 -0500

    feat: hook para obtener la nueva frase

---
+++ src/hooks/useCatFact.js
@@ -0,0 +1,16 @@
+import { getRandomFact } from "../services/catService";
+
+export const useCatFact = () => {
+    const getFact = async () => {
+        try {
+            const newFact = await getRandomFact();
+            return newFact;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    return {
+        getFact
+    };
+};
