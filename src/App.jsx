7b93053e6a2aba8d14463a1f321634cb678e2ac6
Juankyyy <juancamiloherrera960@gmail.com>
2025-05-29 14:46:17 -0500

    feat: botón para cambiar la frase

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Navbar } from "./components/Navbar";
+import { useCatFact } from "./hooks/useCatFact";
 
 export const App = () => {
-    const [fact, setFact] = useState("");
+    const [fact, setFact] = useState("cheese");
+    const { getFact } = useCatFact();
+
+    const changeFact = async () => {
+        const newFact = await getFact();
+        setFact(newFact)
+    }
+
+    useEffect(() => {
+        changeFact();
+    }, [])
 
     return (
         <>
             <Navbar />
             <h1>Cat Facts</h1>
+            <p>{fact}</p>
+            <button onClick={ changeFact }>New fact</button>
         </>
     );
 };
