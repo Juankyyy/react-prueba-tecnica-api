d2322bd6da02c3e5965c072a3fef433593d5b16c
Juankyyy <juancamiloherrera960@gmail.com>
2025-06-01 00:36:15 -0500

    feat: test para verificar la frase

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,10 +3,15 @@ import { test, expect } from "@playwright/test";
 
 const LOCALHOST_URL = "http://localhost:5173/";
 
-test("app shows a fact of cats and a image", async ({ page }) => {
+test("app shows a title, fact of cats and a image", async ({ page }) => {
     await page.goto(LOCALHOST_URL);
 
     // Verificar si se muestra el título
     const title = page.locator("h1");
     await expect(title).toHaveText("Cat Facts");
+
+    // Verificar si hay alguna frase
+    const fact = await page.locator("p");
+    
+    await expect(fact).toHaveText(/\w+/);
 });
